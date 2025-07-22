5223af1d810f26cda17177fbe01413550c6880b6
Chris Potter <aichris@anaiyou.com>
2025-07-22 08:00:19 -0700

    🔧 FORCE REAL CHANGE: Update comment to trigger Netlify rebuild with latest settings

--- src/pages/NewSalesPage.tsx
+++ src/pages/NewSalesPage.tsx
@@ -502,7 +502,7 @@ const YourFigmaHero: React.FC = () => {
                   {/* Speaker and camera notch */}
                   <div className="absolute top-2 left-1/2 -translate-x-1/2 w-24 h-3 bg-gray-800 rounded-full opacity-70 z-10"></div>
                   <div className="absolute top-2 right-8 w-3 h-3 bg-gray-700 rounded-full opacity-70 z-10"></div>
-                  {/* Demo app iframe - LATEST VERSION WITH FIXES */}
+                  {/* Demo app iframe - LATEST VERSION WITH FIXES - FORCE NETLIFY UPDATE */}
                   <iframe
                     src="/#/demo"
                     title="Live Demo"
@@ -716,7 +716,7 @@ const UnifiedFeaturesSection: React.FC = () => {
                   {/* Speaker and camera notch */}
                   <div className="absolute top-2 left-1/2 -translate-x-1/2 w-24 h-3 bg-gray-800 rounded-full opacity-70 z-10"></div>
                   <div className="absolute top-2 right-8 w-3 h-3 bg-gray-700 rounded-full opacity-70 z-10"></div>
-                  {/* Demo app iframe - LATEST VERSION WITH FIXES */}
+                  {/* Demo app iframe - LATEST VERSION WITH FIXES - FORCE NETLIFY UPDATE */}
                   <iframe
                     src="/#/demo"
                     title="Live Demo"
